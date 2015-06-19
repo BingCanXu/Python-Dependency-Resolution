c678d92a36b516fc776b663a29092badbaf20c4e
BingCanXu <bing4130@gmail.com>
2015-06-19 11:14:13 -0400

    Create string_modify.py

---
+++ string_modify.py
@@ -0,0 +1,36 @@
+def setup_string_modifications(oneString): 
+  aStrings = oneString
+  # Some packages has requirements like [Django,] or {Django,}
+  if ',]' in aStrings:
+    aStrings = aStrings.replace(',]', ']')
+	
+  if ']' in aStrings:
+    aStrings= aStrings[0:aStrings.find(']')]
+
+  if ',}' in aStrings:
+    aStrings = aStrings.replace(',}', '}')
+	
+  if '}' in aStrings:
+    aStrings= aStrings[0:aStrings.find('}')]
+  
+  
+  # Checks for cases where the string only contains \r or \n and replaces ,\n with a space
+  aStrings = aStrings.replace(',\n',' ')
+  if(len(aStrings) < 3):
+    aStrings = aStrings.replace('\n',"")
+
+  aStrings = aStrings.replace('\r',"")
+ 
+  # Replaces the extra quotes and comma from install_requires or requires
+  newString = aStrings.replace("','", ' ')
+  newStringw = newString.replace('","', ' ')
+  newStringr = newStringw.replace("\',\"", ' ')
+  newStringt = newStringr.replace("\",\'", ' ')
+  newStringv = newStringt.replace('"','')
+  finString = newStringv.replace("'","")
+  
+  return finString			
+
+
+						
+					
