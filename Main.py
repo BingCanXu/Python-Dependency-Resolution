d455ca94e8fc2f932f3fb67e211a5604ba8682fb
BingCanXu <bing4130@gmail.com>
2015-06-19 15:21:17 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,63 +15,66 @@ counter = 0
 
 for dirpath,dirnames, files in os.walk(topdir):
   for name in files:
-
-    if name == 'setup.py':
-      print counter
-      counter = counter + 1
-      packageName = dirpath[dirpath.rfind('/')+1:len(dirpath)]
-      # Should return none if there is no requires.txt.
-      req_string = getReq.get_from_require(os.path.join(dirpath,''))
-      if req_string:
-        req_list = req_string.split()
-     
-      else: 
-	try:
-	  dataFile = file(os.path.join(dirpath,name))
-	except Exception:
-	  problematicPackages.append(packageName)
-	  continue
-        req_string = getSetup.get_from_setup(dataFile,packageName)
+    # Stop from looking within package for other setup.py
+    if(os.path.join(dirpath,name).count("/") < 10):	
+	    if name == 'setup.py':
+	      counter = counter + 1
+	      packageName = dirpath[dirpath.rfind('/')+1:len(dirpath)]
+	      # Should return none if there is no requires.txt.
+	      req_string = getReq.get_from_require(os.path.join(dirpath,''))
+	      if req_string:
+		req_list = req_string.split()
+	     
+	      else: 
+		try:
+		  dataFile = file(os.path.join(dirpath,name))
+		except Exception:
+		  problematicPackages.append(packageName)
+		  continue
+		req_string = getSetup.get_from_setup(dataFile,packageName)
 	
-	if req_string:
-          if '(' in req_string:		
-          # Make sure it's not numpy(<=1.2) before going to check requirements.txt
-            if not (req_string[req_string.find('(')+1]=='<' or \
-            req_string[req_string.find('(')+1]=='>' or \
-            req_string[req_string.find('(')+1]=='='):
-              req_string = checkRequirements.get_requirements(os.path.join(dirpath,''))
+		if req_string:
+		  if '(' in req_string:		
+		  # Make sure it's not numpy(<=1.2) before going to check requirements.txt
+		    if not (req_string[req_string.find('(')+1]=='<' or \
+		    req_string[req_string.find('(')+1]=='>' or \
+		    req_string[req_string.find('(')+1]=='='):
+		      req_string = checkRequirements.get_requirements(os.path.join(dirpath,''))
 
-	    if req_string: 
+		    if req_string: 
+		      
+		      req_list = req_string.split()
+		
+		    else:
+		      req_list = []
+
+		  else:
+		    req_list = req_string.split() 
+
+		else:
+		  req_list = [] 
+	   
+	      # Package name is key and list of dependency is value
+	      dict_value = []
+	      for u in req_list:
+		dict_value.append(u)
+	      reqs[packageName] = dict_value
 	      
-              req_list = req_string.split()
-        
-            else:
-	      req_list = []
 
-	  else:
-            req_list = req_string.split() 
-
-	else:
-          req_list = [] 
-   
-      # Package name is key and list of dependency is value
-      dict_value = []
-      for u in req_list:
-        dict_value.append(u)
-      reqs[packageName] = dict_value
-
-      # Dictionary with key as required package
-      for u in req_list:
-        # Check if key already exists else creates one
-        if u in prereqs:
-          if packageName not in prereqs[u]:
-            prereqs[u].append(packageName)
-        else:
-	  prereqs[u] = []
-          prereqs[u].append(packageName)
-
-      print packageName
-      print req_list
+	      # Dictionary with key as required package
+	      for u in req_list:
+		# Check if key already exists else creates one
+		if u in prereqs:
+		  if packageName not in prereqs[u]:
+		    prereqs[u].append(packageName)
+		else:
+		  prereqs[u] = []
+		  prereqs[u].append(packageName)
+	      print counter
+	      
+	      print packageName
+	      print req_list
+      
      
 print counter
 with open("probGetPak.txt",'w') as probPak:
