da4f99fdac2939e926e167f043c01f55d3a21f44
BingCanXu <bing4130@gmail.com>
2015-06-22 12:52:21 -0400

    Update checkRequirements.py

--- checkRequirements.py
+++ checkRequirements.py
@@ -9,12 +9,12 @@ def get_requirements(topdir):
 		if name == 'requirements.txt':
 		 reqments_file = file(os.path.join(dirpath,name))
                  #Checks for cases where there are multiple requirements.txt file and it redirects to another folder
-		 # Since reqfile.read() already read the file any more reading would generate '' because it's already eof
-                 if not 'requirements.txt' in reqfile.read().replace(' ',""):
+		 # Since reqments_file.read() already read the file any more reading would generate '' because it's already eof
+                 if not 'requirements.txt' in reqments_file.read().replace(' ',""):
 	           # Re-opens the file so we can read it
                    reqments_file.close()
-		   reqfile = file(os.path.join(dirpath,name))
-	         for aline in reqfile:
+		   reqments_file = file(os.path.join(dirpath,name))
+	         for aline in reqments_file:
 		   if '#' in aline:
 	             # Splice string to not read anything after #.
 		     aline = aline[0:aline.find('#')]
