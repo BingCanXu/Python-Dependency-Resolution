18e6483756bcac195cb395de744db0c0457fe8a8
BingCanXu <bing4130@gmail.com>
2015-06-29 13:29:33 -0400

    Update checkRequirements.py

--- checkRequirements.py
+++ checkRequirements.py
@@ -18,19 +18,19 @@ def get_requirements(topdir):
         try:
           if name == 'requirements.txt':
             reqments_file = open(os.path.join(dirpath,name))
-          # Checks for cases where there are multiple requirements.txt file and it redirects to another folder
-          # Since reqments_file.read() already read the file any more reading would generate '' because it's already eof
-          if not 'requirements.txt' in reqments_file.read().replace(' ',""):
-            # Re-opens the file so we can read it
-            reqments_file.close()
-            reqments_file = open(os.path.join(dirpath,name))
-          for aline in reqments_file:
-            if '#' in aline:
-              # Splice string to not read anything after #.
-              aline = aline[0:aline.find('#')]
-            # Checks for cases for requirements of [test] or [dev]. 
-            if '[' in aline:
-              break
+            # Checks for cases where there are multiple requirements.txt file and it redirects to another folder
+            # Since reqments_file.read() already read the file any more reading would generate '' because it's already eof
+            if not 'requirements.txt' in reqments_file.read().replace(' ',""):
+              # Re-opens the file so we can read it
+              reqments_file.close()
+              reqments_file = open(os.path.join(dirpath,name))
+            for aline in reqments_file:
+              if '#' in aline:
+                # Splice string to not read anything after #.
+                aline = aline[0:aline.find('#')]
+              # Checks for cases for requirements of [test] or [dev]. 
+              if '[' in aline:
+                break
             req_string = req_string + aline
         except Exception:
           continue
