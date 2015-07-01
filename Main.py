6d05c63e7170b4fc66b84f56c27cb99550db5b60
BingCanXu <bing4130@gmail.com>
2015-07-01 12:13:22 -0400

    Changed all the tabs into space
    Also changed from replacing dashes, underscore, and white space with empty space to using
    pkg_resources's safe_name

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import json
 import getReq
 import getSetup
 import checkRequirements
+import pkg_resources
+
 
 '''This script will walk through the directory '/srv/pypi/web/packages/source' 
 and find the dependencies from either the requires.txt, setup.py, or 
@@ -31,102 +33,100 @@ for dirpath,dirnames, files in os.walk(topdir):
       if name == 'setup.py':
         # The name of the package
         packageName = dirpath[dirpath.rfind('/')+1:len(dirpath)]
-        dirpathCpy = dirpath.replace('-','')
-        dirpathCpy = dirpathCpy.replace('_','')
-        dirpathCpy = dirpathCpy.replace(' ','')
-        dirpathCpy = dirpathCpy.lower()
+        dirpathCpy = dirpath.lower()
         splicedstring = dirpathCpy[:dirpathCpy.rfind('/')]
         #The name of the directory that contain the package 
         packageDir = splicedstring[splicedstring.rfind('/')+1:len(splicedstring)]
-        # Copy of packageName without '-', '_', and all lowercase
         packageNamesub = dirpathCpy[dirpathCpy.rfind('/')+1:len(dirpathCpy)]
+        # Normalize the name by replacing non-alphanumerics with '-'
+        packageDir = pkg_resources.safe_name(packageDir)
+        packageNamesub = pkg_resources.safe_name(packageNamesub)
         # Checks for potential errors because standard naming: Numpy/Numpy-1.2 
         # where Numpy = package directory and Numpy-1.2 is packageName
         if packageDir not in packageNamesub:
-      	  weirdCases.append(os.path.join(dirpath,name))
-	  break
+          weirdCases.append(os.path.join(dirpath,name))
+          break
         counter = counter + 1
         # Should return '' if there is no requires.txt.
         req_string = getReq.get_from_require(os.path.join(dirpath,''))
         if req_string:
-      	  req_list = req_string.split()
-     
+          req_list = req_string.split()
+
         else: 
-      	  try:
+          try:
             dataFile = open(os.path.join(dirpath,name))
-      	  except Exception:
-      	    problematicPackages.append(packageName)
-      	    continue
-      	  # Should return the dependencies or (check requirements.txt) if can't find requires
-      	  req_string = getSetup.get_from_setup(dataFile,packageName)
-        
-      	  if req_string:
-      	    # Check to see if it's a method or (check requirements.txt)
-      	    if '(' in req_string:		
-      	    # Make sure it's not numpy(<=1.2) before going to check requirements.txt
-      	      if not (req_string[req_string.find('(')+1]=='<' or \
-      	      req_string[req_string.find('(')+1]=='>' or \
-      	      req_string[req_string.find('(')+1]=='='):
-      	        req_string = checkRequirements.get_requirements(os.path.join(dirpath,''))
-      	      if req_string: 
-      	        req_list = req_string.split()
-      	      else:
-      	        req_list = []
-      	    else:
-      	      req_list = req_string.split() 
-      	  else:
-      	    req_list = [] 
-      	   
-      	# Package name is key and list of dependency is value
-      	dict_value = []
-      	if packageName in reqs:
-      	  weirdCases.append(packageName)
-      	for u in req_list:
-      	  dict_value.append(u)
+          except Exception:
+            problematicPackages.append(packageName)
+            continue
+          # Should return the dependencies or (check requirements.txt) if can't find requires
+          req_string = getSetup.get_from_setup(dataFile)
+          if req_string:
+            # Check to see if it's a method or (check requirements.txt)
+            if '(' in req_string:		
+              # Make sure it's not numpy(<=1.2) before going to check requirements.txt
+              if not (req_string[req_string.find('(')+1]=='<' or \
+              req_string[req_string.find('(')+1]=='>' or \
+              req_string[req_string.find('(')+1]=='='):
+                req_string = checkRequirements.get_requirements(os.path.join(dirpath,''))
+              if req_string: 
+                req_list = req_string.split()
+              else:
+                req_list = []
+            else:
+              req_list = req_string.split() 
+          else:
+            req_list = [] 
+
+        # Package name is key and list of dependency is value
+        dict_value = []
+        if packageName in reqs:
+          weirdCases.append(packageName)
+        for u in req_list:
+          dict_value.append(u)
         reqs[packageName] = dict_value
-      
-      	# Dictionary with key as required package
-      	for u in req_list:
+
+        # Dictionary with key as required package
+        for u in req_list:
           # Check if key already exists else creates one
           if u in prereqs:
-      	    if packageName not in prereqs[u]:
-      	      prereqs[u].append(packageName)
-      	  else:
-      	    prereqs[u] = []
-      	    prereqs[u].append(packageName)
+            if packageName not in prereqs[u]:
+              prereqs[u].append(packageName)
+          else:
+            prereqs[u] = []
+            prereqs[u].append(packageName)
         print counter
         print packageName
         print req_list
-      
-     
+
+
 print counter
 
 with open("weirdcases.txt",'w') as weird:
   for packName in weirdCases:
     weird.write(packName + '\n')
-    
+
 with open("probGetPak.txt",'w') as probPak:
   for packName in problematicPackages:
     probPak.write(packName + '\n')	
-					
+
 with open("reqs.txt",'w') as reqPak:
-	for packList in reqs:
-		reqPak.write(packList + ": ")
-		for packName in reqs[packList]:
-			reqPak.write(' - ' + packName)
-		reqPak.write('\n')	
-		reqPak.write('\n')	
+  for packList in reqs:
+    reqPak.write(packList + ": ")
+    for packName in reqs[packList]:
+      reqPak.write(' - ' + packName)
+    reqPak.write('\n')	
+    reqPak.write('\n')	
 
 with open("prereqs.txt",'w') as prePak:
-	for packList in prereqs:
-		prePak.write(packList + ": ")
-		for packName in prereqs[packList]:
-			prePak.write(' - ' + packName)
-		prePak.write('\n')	
-		prePak.write('\n')	
-			
+  for packList in prereqs:
+    prePak.write(packList + ": ")
+    for packName in prereqs[packList]:
+      prePak.write(' - ' + packName)
+    prePak.write('\n')	
+    prePak.write('\n')	
+
 json.dump(reqs, open("reqjson.txt",'w'))
 json.dump(prereqs, open("prereqjson.txt",'w'))								
-										
+
 
 
